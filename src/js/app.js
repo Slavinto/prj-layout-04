472385ec8efd39d6557ae9eecd086119c8e75567
slavinto <slava3669@gmail.com>
2023-06-30 14:54:21 +0300

    v1.5 implementing auction categories catalogue

--- src/js/app.js
+++ src/js/app.js
@@ -8,5 +8,18 @@ const menuBody = document.querySelector(".menu__body");
 menu.addEventListener("click", () => {
     menu.classList.toggle("_active");
     menuBody.classList.toggle("_active");
+});
 
+const profileIcon = document.querySelector(".profile");
+const profileMenu = document.querySelector(".profile-menu");
+profileIcon.addEventListener("click", (e) => {
+    profileMenu.classList.toggle("_active");
+});
+
+document.addEventListener("click", (e) => {
+    if (
+        !e.target.closest(".profile") &&
+        profileMenu.classList.contains("_active")
+    )
+        profileMenu.classList.remove("_active");
 });
